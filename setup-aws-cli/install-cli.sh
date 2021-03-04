ca5501145006f57044ddf1ae01b1a54cef7a44a5
Swapnil Kulkarni <coolsvap@gmail.com>
2021-03-04 03:27:32 +0000

    Setup aws-cli

---
+++ setup-aws-cli/install-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y install glibc groff
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --update
+rm -rf awscliv2.zip aws
